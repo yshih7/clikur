1381136ca3310622088fa026fe921cb296adeedb
Stephen Cohen <stesen@outlook.com>
2015-11-09 18:18:44 -0500

    Yet another globbing typo fix

--- mobile/build/paths.js
+++ mobile/build/paths.js
@@ -5,7 +5,7 @@ var outputRoot = "build/dest/";
 
 module.exports = {
     root: srcRoot,
-    source: [srcRoot + "*.js", "!" + srcRoot + "config.js", srcRoot + "views/*.js", srcRoot + "js/*.js", "!" + srcRoot + "winstore-jscompat.js"],
+    source: [srcRoot + "*.js", "!" + srcRoot + "config.js", srcRoot + "views/*.js", srcRoot + "js/*.js", "!" + srcRoot + "js/winstore-jscompat.js"],
     html: srcRoot + "**/*.html",
     css: srcRoot + "**/*.css",
     res: [srcRoot + "img/**", srcRoot + "res/**", srcRoot + "jspm_packages/**/!*.ts", srcRoot + "js/winstore-jscompat.js", srcRoot + "config.js"],
