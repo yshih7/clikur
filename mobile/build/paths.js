ae0983fa69dd20c7d9f5735305758ad1a301a220
Stephen Cohen <stesen@outlook.com>
2015-11-09 00:23:08 -0500

    Correct globbing mistake re: winstore-jscompat
    Had the negated globs for the Windows Store Dynamic Content Shim in the
    wrong place, causing Babel to try to "compile" it.

--- mobile/build/paths.js
+++ mobile/build/paths.js
@@ -5,10 +5,10 @@ var outputRoot = "build/dest/";
 
 module.exports = {
     root: srcRoot,
-    source: [srcRoot + "*.js", "!" + srcRoot + "winstore-jscompat.js", srcRoot + "views/*.js", srcRoot + "js/*.js"],
+    source: [srcRoot + "*.js", srcRoot + "views/*.js", srcRoot + "js/*.js", "!" + srcRoot + "winstore-jscompat.js"],
     html: srcRoot + "**/*.html",
     css: srcRoot + "**/*.css",
-    res: [srcRoot + "img/**", srcRoot + "res/**", srcRoot + "jspm_packages/**", "!" + srcRoot + "jspm_packages/**.ts", srcRoot + "winstore-jscompat.js"],
+    res: [srcRoot + "img/**", srcRoot + "res/**", srcRoot + "jspm_packages/**", "!" + srcRoot + "jspm_packages/**.ts", srcRoot + "js/winstore-jscompat.js"],
     destRoot: outputRoot,
     srcOutput: outputRoot + "www/",
     localBuild: outputRoot,
