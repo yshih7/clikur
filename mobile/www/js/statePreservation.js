0261db27371c680b9fd318811ab3276b6bb173fa
Stephen Cohen <stesen@outlook.com>
2015-11-10 18:00:25 -0500

    Refactor statePreservation decorators
    Refactor needsPreservation() to implement/wrap the activate callback
    
    Fix preserve() to properly wrap the initializer for the property

--- mobile/www/js/statePreservation.js
+++ mobile/www/js/statePreservation.js
@@ -1,54 +1,63 @@
 export function needsPreservation(id) {
     return function(target)
     {
-        if ("WinJS" in window) {
-            return class extends target
+        if ("WinJS" in window)
+        {
+            let _activate = target.prototype.activate;
+            target.prototype.activate = function()
             {
-                constructor()
+                var store = window.preservationStore;
+                store.id = id;
+                store.src = this;
+                console.log("Store Configured: %O", store);
+
+                if (store.rehydrate && store.rehydrate.id === id)
                 {
-                    super(...arguments);
-
-                    var store = window.preservationStore;
-                    store.id = id;
-                    store.src = this;
-
-                    if (store.rehydrate && store.rehydrate.id === id)
+                    console.log("Found previous values to rehydrate");
+                    for (let item of store.rehydrate.values)
                     {
-                        console.log("Found previous values to rehydrate");
-                        for (let item of store.rehydrate.values)
-                        {
-                            console.log("%O", item);
-                            if (item.setter) {
-                                this[item.key](item.val);
-                            }
-                            else {
-                                this[item.key] = item.val;
-                            }
+                        console.log("%O", item);
+                        if (item.setter) {
+                            this[item.key](item.val);
                         }
-
-                        store.rehydrate.id = null;
-                        store.rehydrate.values = null;
+                        else {
+                            this[item.key] = item.val;
+                        }
                     }
+
+                    store.rehydrate.id = null;
+                    store.rehydrate.values = null;
+                }
+                
+                if (typeof _activate === "function") {
+                    return _activate.apply(this, arguments);
                 }
             };
         }
-        else {
-            return target;
-        }
+        
+        return target;
     };
 }
     
 export function preserve(setter) {
     return function(target, key, desc) {
-        if ("WinJS" in window) {
-            console.log("Registering " + key + " to preserve");
-            window.preservationStore.preserve.push({
-                key,
-                setter: setter || null
-            });
+        if ("WinJS" in window)
+        {
+            let _init = desc.initializer;
+            desc.initializer = function()
+            {
+                console.log("Registering " + key + " to preserve");
+                window.preservationStore.preserve.push({
+                    key,
+                    setter: setter || null
+                });
+                if (typeof _init === "function") {
+                    return _init(...arguments);
+                }
+            };
         }
         
-        //console.log("Desc: %O", desc);
+        //Babel bug? Anyway, this is necessary or the prop is magically read-only
         desc.writable = true;
         return desc;
     };
