5747627caeee3d9707ade3001b8ed080904f8573
Stephen Cohen <stesen@outlook.com>
2015-11-12 21:35:49 -0500

    Start of Home screen

---
+++ mobile/www/js/Course.js
@@ -0,0 +1,49 @@
+export class Course
+{
+    //start-es7
+    name: string; //e.g., "Intro to HCI"
+    callSign: string; //e.g., "CSC212"
+    session: Session;
+    id: numeric;
+    //end-es7
+
+    constructor(name, callSign, session, id)
+    {
+        this.name = name;
+        this.callSign = callSign;
+        this.session = session;
+        this.id = id;
+    }
+}
+
+export class Session //jshint ignore:line
+{
+    //start-es7
+    static daysRegex = /^(?!$)M?W?T?R?F?S?U?$/;
+    
+    days: string;
+    startTime: Time;
+    endTime: Time;
+    //end-es7
+
+    constructor(days, startTime, endTime)
+    {
+        this.days = days;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
+
+export class Time //jshint ignore:line
+{
+    //start-es7
+    hour: numeric;
+    min: numeric;
+    //end-es7
+
+    constructor(hour, min)
+    {
+        this.hour = hour;
+        this.min = min;
+    }
+}
---
+++ mobile/www/views/home.js
@@ -0,0 +1,6 @@
+export class Home
+{
+    //start-es7
+    courses = [];
+    //end-es7
+}
