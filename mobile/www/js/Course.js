7eda598409e8d0cc0a8be176cbb2599bc65607d7
Stephen Cohen <stesen@outlook.com>
2015-11-13 23:19:08 -0500

    Add basic value validation to Course classes

--- mobile/www/js/Course.js
+++ mobile/www/js/Course.js
@@ -28,6 +28,10 @@ export class Session
 
     constructor(days, startTime, endTime)
     {
+        if (!Session.daysRegex.test(days)) {
+            throw new Error(`Invalid days string "${days}"`);
+        }
+        
         this.days = days;
         this.startTime = startTime;
         this.endTime = endTime;
@@ -43,6 +47,10 @@ export class Time
 
     constructor(hour, min)
     {
+        if (hour < 0 || hour > 23 || min < 0 || min > 59) {
+            throw new Error(`Invalid time "${hour}:${min}"`);
+        }
+        
         this.hour = hour;
         this.min = min;
     }
