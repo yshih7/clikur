ce434a01c9075c8deb1453aa00229aa171c2c680
Shuyang <sliu44@u.rochester.edu>
2015-11-18 17:09:21 -0500

    add font to the app

--- mobile/build/paths.js
+++ mobile/build/paths.js
@@ -8,7 +8,7 @@ module.exports = {
     source: [srcRoot + "*.js", "!" + srcRoot + "config.js", srcRoot + "view-models/*.js", srcRoot + "js/*.js", "!" + srcRoot + "js/winstore-jscompat.js"],
     html: [srcRoot + "**/*.html", "!" + srcRoot + "jspm_packages/**"],
     css: [srcRoot + "**/*.css", "!" + srcRoot + "jspm_packages/**"],
-    res: [srcRoot + "img/**", srcRoot + "res/**", srcRoot + "jspm_packages/**/*.js", srcRoot + "js/winstore-jscompat.js", srcRoot + "config.js"],
+    res: [srcRoot + "img/**", srcRoot + "res/**", srcRoot + "jspm_packages/**/*.js", srcRoot + "js/winstore-jscompat.js", srcRoot + "config.js", srcRoot + "**/*.woff", srcRoot + "**/*.woff2"],
     destRoot: outputRoot,
     srcOutput: outputRoot + "www/",
     localBuild: outputRoot
