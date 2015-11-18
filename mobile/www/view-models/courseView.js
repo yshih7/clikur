06dcb0a3adca3e03a1ef05e0a0b0063c4bd395a2
Stephen Cohen <stesen@outlook.com>
2015-11-17 22:46:55 -0500

    Implement course-view custom element
    Implements a new custom element called course-view that can be bound to
    a course and will display an item for it. Will optionally show a delete
    button if a function is bound to del-action.

---
+++ mobile/www/view-models/courseView.js
@@ -0,0 +1,21 @@
+import {customElement, bindable} from "aurelia-framework"; //jshint ignore:line
+
+//start-es7
+@customElement("course-view")
+//end-es7
+export class CourseView
+{
+    //start-es7
+    course: Course;
+    @bindable
+    index: numeric;
+    @bindable
+    delAction = null; //function(numeric): undefined
+    //end-es7
+
+    _delAction(e)
+    {
+        e.stopPropagation();
+        this.delAction(this.index);
+    }
+}
