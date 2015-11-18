83a3b35f3106e74a37963e3c4bd8ada1cac2d2e7
thnichols <thnichols@gmail.com>
2015-11-17 21:06:08 -0500

    Generated Routes
    Created the URL path structure for the site.

--- web/clikur-site/app/router.js
+++ web/clikur-site/app/router.js
@@ -6,6 +6,18 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('login');
+  this.route('register');
+  this.route('courses', function() {
+    this.route('add');
+  });
+  this.route('course', { path: 'course/:course-id' }, function() {
+    this.route('quiz', { path: 'quiz/:quiz-id' });
+    this.route('quizzes', function() {
+      this.route('add');
+    });
+  });
+  this.route('lecture', { path: 'lecture/:course-id' });
 });
 
 export default Router;
