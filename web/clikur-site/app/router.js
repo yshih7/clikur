4f11dd775018ffa3c51248c2e8cfac757619ef86
thnichols <thnichols@gmail.com>
2015-11-20 20:31:44 -0500

    Updated CSS and /courses/add.hbs

--- web/clikur-site/app/router.js
+++ web/clikur-site/app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login');
+  this.route('login', { path: '/' });
   this.route('register');
   this.route('courses', function() {
     this.route('add');
