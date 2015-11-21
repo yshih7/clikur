14af046a63dd1986d5fbd6e289dfe11157496592
Stephen Cohen <stesen@outlook.com>
2015-11-21 02:43:51 -0500

    Skeleton of course home screen
    Added basic outlines for the course home screen. Various new custom
    elements and data structures will go into this screen.

---
+++ mobile/www/view-models/courseHome.js
@@ -0,0 +1,20 @@
+import {inject} from "aurelia-framework";
+import {UserData} from "js/UserData";
+
+//start-es7
+@inject(UserData)
+//end-es7
+export class CourseHome {
+    //start-es7
+    course;
+    userData: UserData;
+    //end-es7
+    
+    constructor(userData) {
+        this.userData = userData;
+    }
+    
+    activate(params) {
+        this.course = this.userData.courseList.get(params.cid);
+    }
+}
