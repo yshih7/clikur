b1d4b54e2e21cbf11072f43f6e4149c8afd53194
Stephen Cohen <stesen@outlook.com>
2015-11-21 19:09:32 -0500

    Clean up questionBox.js
    Noticed a few more little mistakes and cleaned up the file.

--- mobile/www/view-models/questionBox.js
+++ mobile/www/view-models/questionBox.js
@@ -1,47 +1,53 @@
-import {customElement, bindable} from "aurelia-framework";
-import {Router} from 'aurelia-router';
+import {customElement, bindable} from "aurelia-framework"; //jshint ignore:line
+import {Router} from 'aurelia-router'; //jshint ignore:line
 
+//start-es7
 @customElement("question-box")
-@inject('Router')
+@inject(Router)
+//end-es7
 export class QuestionBox
 {
-  @bindable
-  questionMap : Map;
+    //start-es7
+    @bindable
+    questionMap : Map;
 
-  show_list : boolean;
+    show_list : boolean;
 
-  @bindable
-  route_name : String;
+    @bindable
+    route_name : String;
 
-  @bindable
-  course_id : numeric;
+    @bindable
+    course_id : numeric;
+    //end-es7
 
-  constructor(router)
-  {
-    this.router = router;
-    //by default, the question list is not shown
-    show_list = false;
-  }
-
-  show_q_detail(id)
-  {
-    //link to the question detail page
-    this.router.navigate(route_name,{cid:course_id,qid:id});
-  }
-  zoom()
-  {
-    show_list = !show_list;
-    if(show_list)
+    constructor(router)
     {
-      document.removeEventListener('backbutton',document.backListener,false);
-      this.backListener = ()=>{this.zoom();};
-      document.addEventListener('backbutton',this.backListener,false);
+        this.router = router;
+        //by default, the question list is not shown
+        this.show_list = false;
     }
-    else
-    {
-      document.removeEventListener('backbutton',this.backListener,false);
-      document.addEventListener('backbutton',document.backListener,false);
-    }
-  }
 
+    show_q_detail(id)
+    {
+        //link to the question detail page
+        this.router.navigate(this.route_name, {
+            cid: this.course_id,
+            qid:id
+        });
+    }
+    zoom()
+    {
+        this.show_list = !this.show_list;
+        if(this.show_list)
+        {
+            document.removeEventListener('backbutton', document.backListener, false);
+            this.backListener = () => this.zoom();
+            document.addEventListener('backbutton', this.backListener, false);
+        }
+        else
+        {
+            document.removeEventListener('backbutton', this.backListener, false);
+            document.addEventListener('backbutton', document.backListener, false);
+        }
+    }
 }
