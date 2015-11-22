1b4596f8d73e80666ddff17e08c9c587c2c243bb
thnichols <thnichols@gmail.com>
2015-11-22 14:53:15 -0500

    /course and /courses index pages

--- web/clikur-site/app/routes/courses.js
+++ web/clikur-site/app/routes/courses.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		let courses = [
+			{
+				course: "MTH 161",
+				title: "Calculus IA",
+				crn: "29288",
+				term: "Fall 2016"
+			},
+			{
+				course: "CHM 131",
+				title: "CHM Concepts, Systems, Practice I",
+				crn: "25954",
+				term: "Fall 2016"
+			}
+		];
+		return courses;
+	}
 });
