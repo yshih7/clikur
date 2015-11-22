fb5985e0437b13aa3a8a97bde06bbd027f841137
Stephen Cohen <stesen@outlook.com>
2015-11-22 03:26:52 -0500

    Clean up answerQuestion
    Fixed a bunch of confusions and typos.
    Rearranged the HTML a bit.
    Bound the radio buttons to the index of the selected option, rather than
    the text.

--- mobile/www/view-models/answerQuestion.js
+++ mobile/www/view-models/answerQuestion.js
@@ -1,7 +1,8 @@
 import {needsPreservation, preserve} from "js/statePreservation"; //jshint ignore:line
-import {inject} from "aurelia-framework";
-import {UserData} from "js/UserData";
+import {inject} from "aurelia-framework"; //jshint ignore:line
+import {UserData} from "js/UserData"; //jshint ignore:line
 import {QuizQuestion} from "js/QuizQuestion";
+/*globals Camera*/
 
 //start-es7
 @inject(UserData)
@@ -9,56 +10,64 @@ import {QuizQuestion} from "js/QuizQuestion";
 //end-es7
 export class AnswerQuestion
 {
-	//Initialize the variables for binding
-	constructor(UserData) {
-		this.UserData = UserData;
-		this.showText = false;
-		this.showRadio = true;
-		this.showImage = false;
-    	this.choices = ['Q1', 'Q2', 'Q3', 'Q4'];
-		this.sometext; //Text in the text input field
-		this.questionText = 'Sample Question';
-		this.selectedChoice;
-  	}
-	
-    @preserve()
-    image1 = "img/tinyblank.gif";
+    //start-es7
+    userData: UserData;
+    course: Course;
+    question: QuizQuestion;
+    showText: boolean = false;
+    showRadio: boolean = false;
+    showImage: boolean = false;
 
-	activate(params){
+    @preserve()
+    textInput: string;
+    @preserve()
+    selection: number;
+    @preserve()
+    imageData: string = "img/tinyblank.gif";
+    //end-es7
+    
+	//Initialize the variables for binding
+	constructor(userData) {
+		this.userData = userData;
+  	}
+
+	activate(params)
+    {
 		this.course = this.UserData.courseList.get(params.cid);
 		this.question = this.course.quizQuestions.get(params.qid);
+        
+        var types = QuizQuestion.questionTypes;
+        if (this.question.type === types.TEXT) {
+            this.showText = true;
+        }
+        else if (this.question.type === types.MULTI) {
+            this.showRadio = true;
+        }
+        else {
+            this.showImage = true;
+        }
 	}
-	
-	//Syncing all the variables used in this file with the QuizQuestion
-	//object obtained from user data
-	this.showText = (this.question.questionTypes.TEXT === this.question.type);
-	this.showRadio = (this.question.questionTypes.MULTI === this.question.type);
-	this.showImage = (this.question.questionTypes.IMG === this.question.type);
-	this.choices = 	this.question.choices;
-	this.questionText = this.question.text;
 
 	//When the take picture button is clicked
-	takePicture(){
-		navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-		destinationType: Camera.DestinationType.DATA_URL
+	takePicture()
+    {
+        var onSuccess = imageData => this.image = `data:image/jpeg;base64,${imageData}`;
+
+		var onFail = message => navigator.notification.alert(`Image capture failed. Reason: "${message}"`, null, "Error");
+        
+		navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 45, //Has to be less than 50 for iOS compat
+            destinationType: Camera.DestinationType.DATA_URL,
+            allowEdit: false, //Doesn't work right on Android
+            saveToPhotoAlbum: false
 		});
-
-		function onSuccess(imageData) {
-			//var image = document.getElementById('answerImage');
-			image1 = "data:image/jpeg;base64," + imageData;
-		}
-
-		function onFail(message) {
-			//alert('Failed because: ' + message);
-			//notification
-		}
 	}
 
     submitAnswer() {
-		if(showText){
+		if(this.showText){
 			//TODO Submit text
 			//return this.sometext;
-		} else if(showRadio){
+		} else if(this.showRadio){
 			//TODO Submit radio choice
 			// return this.selectedChoice;
 		} else {
