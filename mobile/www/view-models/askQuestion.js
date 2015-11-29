262f261e46485fdb668c0373f9e546bd8444980b
Stephen Cohen <stesen@outlook.com>
2015-11-29 16:48:03 -0500

    Clean up askQuestion visuals
    Databound a dynamic title in a proper header and cleaned up the CSS

--- mobile/www/view-models/askQuestion.js
+++ mobile/www/view-models/askQuestion.js
@@ -2,35 +2,47 @@ import {needsPreservation, preserve} from "js/statePreservation"; //jshint ignor
 import {Router} from 'aurelia-router'; //jshint ignore:line
 import {inject} from "aurelia-framework"; //jshint ignore:line
 import {UserQuestion} from "js/UserQuestion";
+import {UserData} from "js/UserData"; //jshint ignore:line
 
 //start-es7
-@inject(Router)
+@inject(Router, UserData)
 @needsPreservation("askQuestion")
 //end-es7
 export class askQuestion
 {
     //start-es7
-	@preserve()
-	question;	// can access by this.question
-	@preserve()
-	anonymous;
+	@preserve() //Still need to serialize this directly for WP because we can't serialize on every edit
+	question: UserQuestion;	// can access by this.question
+    course: Course;
+    editMode: boolean = false;
     //end-es7
 
-	constructor(router)
+	constructor(router, userData)
 	{
 		this.router = router;
+        this.userData = userData;
 	}
 
 	submitQuestion(event)
 	{
-		var time = event.timeStamp;
-		var ques = new UserQuestion(time, this.question, this.anonymous, false);
+		this.question.timestamp = event.timeStamp;
+        //TODO: Send to server
+        
 		//navigate back to student home
 		this.router.navigateBack();
 	}
     
     activate(params)
     {
+        this.course = this.userData.courseList.get(+params.cid);
         
+        if (params.qid)
+        {
+            this.editMode = true;
+            this.question = this.course.userQuestions.get(+params.qid);
+        }
+        else {
+            this.question = new UserQuestion(null, "", false, false);
+        }
     }
 }
