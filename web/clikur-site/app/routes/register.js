6c60164653d511afdda8e50cc8ea63eccbef502b
thnichols <thnichols@gmail.com>
2015-11-28 20:47:50 -0500

    Moved JavaScript to .js file for register

--- web/clikur-site/app/routes/register.js
+++ web/clikur-site/app/routes/register.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
+			
+var submitInfo = function () {
+	//try submitting, if database rejects the info because of an invalid email return false, else true if successful
+	return true;
+}
 
 export default Ember.Route.extend({
+	actions: {
+		checkConfirmations: function () {
+			var email1 = document.getElementById("email").value;
+			var email2 = document.getElementById("confEmail").value;
+			
+			var pass1 = document.getElementById("password").value;
+			var pass2 = document.getElementById("confPassword").value;
+			
+			var infoCorrect = true; //determines whether to submit or not
+			
+			if(email1 !== email2){
+				document.getElementById("emailWarning").innerHTML = "Email does not match";
+				infoCorrect = false;
+			}
+			else{
+				document.getElementById("emailWarning").innerHTML = "";
+			}
+			if(pass1 !== pass2){
+				document.getElementById("passWarning").innerHTML = "Password does not match";
+				infoCorrect = false;
+			}
+			else {
+				document.getElementById("passWarning").innerHTML = "";
+			}
+			
+			if(infoCorrect){
+				var submitSuccessful = submitInfo();
+			}
+			
+			if(submitSuccessful){
+				//navigate to the next page
+			}
+		}
+	}
 });
