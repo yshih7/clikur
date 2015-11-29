918feb346a7000a7e886f217e8d6d7df891c94f2
thnichols <thnichols@gmail.com>
2015-11-29 13:33:43 -0500

    Added start and end times to courses/add

--- web/clikur-site/app/routes/courses/add.js
+++ web/clikur-site/app/routes/courses/add.js
@@ -2,6 +2,34 @@ import Ember from 'ember';
 
 var $ = Ember.$;
 
+function populateOptions(optList, val) {
+	var opt = new Option();
+	opt.text = val;
+	opt.value = val;
+	optList.add(opt);
+}
+
+$(document).ready(function () {
+
+	var startHourOptions = $("#startHour").get(0).options,
+		startMinuteOptions = $("#startMinute").get(0).options,
+		endHourOptions = $("#endHour").get(0).options,
+		endMinuteOptions = $("#endMinute").get(0).options;
+		
+	for (var i = 7; i < 21; i++) {
+		populateOptions(startHourOptions, i);
+		populateOptions(endHourOptions, i);
+	}
+	for (var i = 0; i < 60; i += 5) {
+		var minute = i.toString();
+		if (i < 10) {
+			minute = "0" + i.toString();
+		}
+		populateOptions(startMinuteOptions, minute);
+		populateOptions(endMinuteOptions, minute);
+	}
+});
+
 export default Ember.Route.extend({
 	actions: {
 		setSemester: function () {
@@ -22,6 +50,18 @@ export default Ember.Route.extend({
 				});
 				return sessionVal;
 			});
+		},
+		setStartTime: function () {
+			$("#start_time").val("")
+			.val(
+				$("#startHour").val() + ":" + $("#startMinute").val()
+			);
+		},
+		setEndTime: function () {
+			$("#end_time").val("")
+			.val(
+				$("#endHour").val() + ":" + $("#endMinute").val()
+			);
 		},
 		submit: function () {
 			var isValid = true,
