3d171ffba1cceff8305a046615aadd79c339e99a
thnichols <thnichols@gmail.com>
2015-11-30 15:57:22 -0500

    Moved course.hbs template to course/index.hbs, Bootstrapped course/quizzes/add

--- web/clikur-site/app/routes/course/quizzes/add.js
+++ web/clikur-site/app/routes/course/quizzes/add.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
+var showingChoices = false;
+			
 export default Ember.Route.extend({
+	actions: {
+		showAnswerChoices: function(qtype) {
+			if(showingChoices && (qtype === "other")){
+				showingChoices = false;
+				document.getElementById("choices").style.display = "none";
+			}
+			else if(!showingChoices && (qtype === "mc")){
+				showingChoices = true;
+				document.getElementById("choices").style.display = "block";
+			}
+		}
+	}
 });
