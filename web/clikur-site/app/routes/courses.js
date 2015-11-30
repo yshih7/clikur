8d535caa37be1855d782d7aa526dd75010147ba2
thnichols <thnichols@gmail.com>
2015-11-29 19:21:14 -0500

    Added course example to course list

--- web/clikur-site/app/routes/courses.js
+++ web/clikur-site/app/routes/courses.js
@@ -25,6 +25,14 @@ export default Ember.Route.extend({
 		let courses = [
 			{
 				id: "1",
+				callsign: "REL 164",
+				title: "Death, Dying & Beyond",
+				crn: "71023",
+				semester: "Fall 2015",
+				session: "MW"
+			},
+			{
+				id: "2",
 				callsign: "MTH 161",
 				title: "Calculus IA",
 				crn: "29288",
@@ -32,7 +40,7 @@ export default Ember.Route.extend({
 				session: "MWF"
 			},
 			{
-				id: "2",
+				id: "3",
 				callsign: "CHM 131",
 				title: "CHM Concepts, Systems, Practice I",
 				crn: "25954",
